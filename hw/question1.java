70f79fc73a7425ac1d5ef404c7727de0fd477aa6
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-02-20 21:55:54 -0600

    Adding hw q1

---
+++ hw/question1.java
@@ -0,0 +1,10 @@
+public class question1 {
+    public static void main(String[] args) {
+        // Write a function that does:
+        // 1. Takes in 2 numbers and subtracts the smaller one from the bigger one
+        // i.e. subtractSmallOne(3, 5) == 2
+        // i.e. subtractSmallOne(5, 3) == 2
+        //
+    }
+
+}
