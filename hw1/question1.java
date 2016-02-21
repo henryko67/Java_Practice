fd819163e53be9a5f77b611411a52414d63da98d
henryko67 <henry.ko67@gmail.com>
2016-02-21 17:08:09 +0800

    Attempt at question 1
    Hello, I tried to finish Question 1, could you please tell me if I didn't do it correctly? Thanks.

--- hw1/question1.java
+++ hw1/question1.java
@@ -1,10 +1,16 @@
 public class question1 {
     public static void main(String[] args) {
+        int x = 6;
+        int y = 4;
+        int z = twoNumDifference (x, y);
+        System.out.println("Diff:" +z);
         // Write a function that does:
         // 1. Takes in 2 numbers and subtracts the smaller one from the bigger one
         // i.e. subtractSmallOne(3, 5) == 2
         // i.e. subtractSmallOne(5, 3) == 2
         //
     }
-hello
+    public static int twoNumDifference(int x, int y) {
+        return x - y;
+    }
 }
