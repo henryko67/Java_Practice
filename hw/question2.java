e526ae474ba31eb1de046c1d5fae51f7e412e1b7
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-02-20 22:02:30 -0600

    Wrote another hw question

---
+++ hw/question2.java
@@ -0,0 +1,7 @@
+public class question2 {
+    public static void main(String[] args) {
+        // Write a function that does:
+        // Takes in an integer input n, and finds the largest prime number smaller than n
+    }
+
+}
