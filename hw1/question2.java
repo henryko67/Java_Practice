42b958f7b2f014c0177f4798354c5bbdb602da97
henryko67 <henry.ko67@gmail.com>
2016-02-27 02:19:22 +0800

    Attempt on Question 2
    I tried to solve it on my own after looking at Barron's and your document, but I still couldn't really get it. I did some googling, and I came  up with this code, but do you think you can help me explain what break is and exactly how to use boolean again? Sorry.

--- hw1/question2.java
+++ hw1/question2.java
@@ -2,8 +2,19 @@ public class question2 {
     // This question is going to use a concept we didn't get to cover in class, called a loop. 
     // Before attempting this, please take a look at lec1/loop.java
 
-    public static void main(String[] args) {
-    // Write a function that tells whether the given number is a prime number or not.
-    }
-
+   public static void main(String[] args) {
+		System.out.println("Prime numbers " + 50);
+		for (int i = 1; i < 50; i++) {
+			boolean isPrimeNumber = true;
+			for (int k = 2; k < i; k++) {
+				if (i % k == 0) {
+					isPrimeNumber = false;
+					break;
+				}
+			}
+			if (isPrimeNumber) {
+				System.out.print(i + "");
+			}
+		}
+	}
 }
