3e3aeeb622e96003954439f1ae64b82f21a68935
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-02-28 20:21:22 -0600

    solution for question1

--- hw1/question1.java
+++ hw1/question1.java
@@ -1,16 +1,15 @@
 public class question1 {
     public static void main(String[] args) {
-        int x = 6;
-        int y = 4;
+        int x = 4;
+        int y = 6;
         int z = twoNumDifference (x, y);
         System.out.println("Diff:" +z);
-        // Write a function that does:
-        // 1. Takes in 2 numbers and subtracts the smaller one from the bigger one
-        // i.e. subtractSmallOne(3, 5) == 2
-        // i.e. subtractSmallOne(5, 3) == 2
-        //
     }
     public static int twoNumDifference(int x, int y) {
-        return x - y;
+        if (x > y) { 
+            return x - y;
+        } else {
+            return y - x;
+        }
     }
 }
