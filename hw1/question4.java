ddff3331fd071cb193d6eba828f1f22498f3c4eb
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-02-29 00:55:37 -0600

    Add question 6

--- hw1/question4.java
+++ hw1/question4.java
@@ -1,5 +1,10 @@
-public class question3 {
+public class question4 {
     public static void main(String[] args) {
         // Print all integers that are multiples of 3 or 5 below 200
+        for (int i = 3; i < 200; i++) {
+            if ( i % 3 == 0 || i % 5 == 0 ) {
+                System.out.println(i);
+            }
+        }
     }
 }
---
+++ hw1/question6.java
@@ -0,0 +1,6 @@
+public class question4 {
+    public static void main(String[] args) {
+        // Print all integers that are multiples of 3 or 5 but is NOT a multiple of 15 below 100
+        // i.e. 3, 5, 6, 9, 10, 12, 18 (skip 15 because that's a multiple of 15)
+    }
+}
