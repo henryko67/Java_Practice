77f29cdf544abbedd8ac3235e8752ff554ef84ee
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-02-29 02:09:48 -0600

    Making it into a function

--- hw1/question2.java
+++ hw1/question2.java
@@ -5,16 +5,25 @@ public class question2 {
    public static void main(String[] args) {
 		System.out.println("Prime numbers " + 50);
 		for (int i = 1; i < 50; i++) {
-			boolean isPrimeNumber = true;
-			for (int k = 2; k < i; k++) {
-				if (i % k == 0) {
-					isPrimeNumber = false;
-					break;
-				}
-			}
-			if (isPrimeNumber) {
+			if (isPrime(i)) {
 				System.out.print(i + ", ");
 			}
 		}
 	}
+
+   // This function basically determines whether a number is prime or not
+   public static boolean isPrime(int i) {
+       boolean isPrimeNumber = true;
+       for (int k = 2; k < i; k++) {
+           if (i % k == 0) {
+               isPrimeNumber = false;
+               break;
+           }
+       }
+       if (isPrimeNumber) {
+           return true;
+       } else {
+           return false;
+       }
+   }
 }
